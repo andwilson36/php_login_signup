41a6eb8d879d6336dc4dac0826dbb5ac6944b3fe
Andrew Wilson <wilsonandrew57@gmail.com>
2021-09-28 22:50:14 -0500

    header css

--- header.php
+++ header.php
@@ -1,8 +1,6 @@
 <header>
-    <div class="logo">
-        PHP
-    </div>
-    <ul>
-        <li>Logout</li>
+    <ul class="header-nav">
+        <li class="logo header-left">PHP</li>
+        <li class="header-right">Logout</li>
     </ul>
 </header>
