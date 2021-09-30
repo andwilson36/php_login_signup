1ac882b17870b971fdb0a0db676f737bc948c6f2
Andrew Wilson <wilsonandrew57@gmail.com>
2021-09-29 23:00:56 -0500

    called functions inside signup that will be created in functions.inc

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -7,7 +7,27 @@ if (isset($_POST["submit"])) {
 
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
+
+    if (emptyInputSignup($username, $password, $passwordRepeat) !== false) {
+        header("location: ../signup.php?error=emptyinput");
+        exit();
+    }
+    if (invalidUid($username) !== false) {
+        header("location: ../signup.php?error=invaldUid");
+        exit();
+    }
+    if (passwordMatch($password, $passwordRepeat) !== false) {
+        header("location: ../signup.php?error=matchfail");
+        exit();
+    }
+    if (uidExists($connection, $username) !== false) {
+        header("location: ../signup.php?error=usernametaken");
+        exit();
+    }
+
+    createUser($connection, $username, $password);
 } 
 else {
     header("location: ../signup.php");
+    exit();
 }
